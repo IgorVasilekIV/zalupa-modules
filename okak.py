e02fac70e2ce2c474f5c17ba2692a28e364316e5
Niagara <igorbro49@gmail.com>
2025-01-29 14:39:51 +0700

    Update okak.py

--- okak.py
+++ okak.py
@@ -1,18 +1,34 @@
-# meta banner: https://raw.githubusercontent.com/Fixyres/Modules/refs/heads/main/IMG_20250112_185329_901.jpg
-# meta developer: @Foxy437 & okak(@IgorVasilekIV)
-
+# чатгпт кормит, больные мозги тоже
+#
+# meta banner: https://0x0.st/s/gJtVZxi43-Zy4q2je-yx-A/8XdP.gif
+# meta developer: @IgorVasilekIV
+#
+# The module is made as a joke, all coincidences are random :P
+# 
+#       кот вахуи
+#       /\_____/\
+#      /  o   o  \
+#     ( ==  ^  == )
+#      )         (
+#     (           )
+#    ( (  )   (  ) )
+#   (__(__)___(__)__)
+# 
+# 
+#
+# based on: https://raw.githubusercontent.com/Fixyres/Modules/main/venom.py
 from .. import loader, utils
 
 @loader.tds
-class okak(loader.Module):
-    """окак для @MrAmigoch"""
+class okakMod(loader.Module):
+    """окак"""
 
     strings = {
-        "name": "окак для @MrAmigoch"
+        "name": "окак"
     }
 
     async def okakcmd(self, m):
-        """okak"""
+        """окак"""
         await utils.asyncio.sleep(1)
         self.db.set("okak", "on", not self.db.get("okak", "on", False))
         if self.db.get("okak", "on", False):
