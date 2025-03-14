b66cda53bd8e0a28eb329f5babe29878e34555a1
monke <igorbro49@gmail.com>
2025-03-14 12:58:02 +0700

    Create bootanim.py

---
+++ bootanim.py
@@ -0,0 +1,31 @@
+# чатгпт кормит, больные мозги тоже
+#
+# meta banner: https://files.catbox.moe/nldg9c.jpg
+# meta developer: @HikkaZPM
+#
+# The module is made as a joke, all coincidences are random :P
+# 
+#       кот вахуи
+#       /\_____/\
+#      /  o   o  \
+#     ( ==  ^  == )
+#      )         (
+#     (           )
+#    ( (  )   (  ) )
+#   (__(__)___(__)__)
+# 
+#
+from hikkatl.types import Message
+from .. import loader, utils
+from hikkatl.errors import ChannelPrivateError
+import asyncio
+import logging
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class BootAnimModUPD(loader.Module):
+    """Модуль для создания boot animation через группу (фиксированный ID)"""
+    
+    strings = {"name": "BootAnimUPD"}
+
+# coming soon...
