41192388de7b5052d459218f98d946d66cce72da
igorbro49@gmail.com <igorbro49@gmail.com>
2025-09-09 11:41:05 +0700

    error fix

--- ssend.py
+++ ssend.py
@@ -25,14 +25,14 @@ class SSend(loader.Module):
         "error": "❌ Пожалуйста, укажите текст для отправки.",
     }
 
-   def __init__(self):
-        self.config = loader.ModuleConfig(
-            loader.ConfigValue(
-                "link_preview",
-                False,
-                lambda: "Disable/enable link preview" 
-                validator=loader.validators.Boolean(),
-            ),
+        def __init__(self):
+            self.config = loader.ModuleConfig(
+                loader.ConfigValue(
+                    "link_preview",
+                    False,
+                    lambda: "Disable/enable link preview" 
+                    validator=loader.validators.Boolean(),
+                ),
             )
 
     @loader.command(ru_doc="Отправить текст с эмодзи")
